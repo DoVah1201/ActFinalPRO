b7a232a37c3bfa46c9eea4bd9be86c6be06c557f
Saul <123548756+DoVah1201@users.noreply.github.com>
2025-06-05 22:54:09 +0100

    abstractcrudservice

--- src/main/java/com/actfinal/ut7/src/main/java/com/actfinal/ut7/model/Nota.java
+++ src/main/java/com/actfinal/ut7/src/main/java/com/actfinal/ut7/model/Nota.java
@@ -1,5 +1,24 @@
 package com.actfinal.ut7.src.main.java.com.actfinal.ut7.model;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notas")
 public class Nota {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+@JoinColumn(name = "usuario_id", nullable = false)
+
+private String titulo;
+
+
+private String contenido;
+
+
+private LocalDateTime fechaCreacion;
 
 }
